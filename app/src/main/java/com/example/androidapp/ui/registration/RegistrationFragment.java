6c2a3cbf0175cd44c76f432cabb1b290bc15e50b
Lums1000 <arturfrankowski1000@gmail.com>
2021-05-05 16:08:19 +0200

    login/registration - fixes registration image load added

--- app/src/main/java/com/example/androidapp/ui/registration/RegistrationFragment.java
+++ app/src/main/java/com/example/androidapp/ui/registration/RegistrationFragment.java
@@ -1,13 +1,17 @@
 package com.example.androidapp.ui.registration;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +23,7 @@ import androidx.navigation.Navigation;
 import com.example.androidapp.R;
 import com.example.androidapp.ui.profile.ProfileViewModel;
 
-import java.util.Objects;
+import static android.app.Activity.RESULT_OK;
 
 public class RegistrationFragment extends Fragment {
 
@@ -27,11 +31,18 @@ public class RegistrationFragment extends Fragment {
     private ProfileViewModel profileViewModel;
     private NavController navController;
 
-    private static int RESULT_LOAD_IMAGE = 1;
+    private static final int PICK_IMAGE = 100;
+    private ImageView avatarImage;
+    private Uri imageUri;
 
     public void hideKeyboard(View view) {
         InputMethodManager inputMethodManager = (InputMethodManager)  getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -40,16 +51,19 @@ public class RegistrationFragment extends Fragment {
                 new ViewModelProvider(this).get(RegistrationViewModel.class);
         View root = inflater.inflate(R.layout.fragment_registration, container, false);
 
-        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
         profileViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
 
         EditText login = (EditText) root.findViewById(R.id.registrationPage_login);
         EditText password = (EditText) root.findViewById(R.id.registrationPage_password);
         EditText confPassword = (EditText) root.findViewById(R.id.registrationPage_confPassword);
         EditText email = (EditText) root.findViewById(R.id.registrationPage_email);
-        View.OnFocusChangeListener hideSoftKeyboard = (v, hasFocus) -> {
-            if (!hasFocus) {
-                hideKeyboard(v);
+        View.OnFocusChangeListener hideSoftKeyboard = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
             }
         };
         login.setOnFocusChangeListener(hideSoftKeyboard);
@@ -57,20 +71,23 @@ public class RegistrationFragment extends Fragment {
         confPassword.setOnFocusChangeListener(hideSoftKeyboard);
         email.setOnFocusChangeListener(hideSoftKeyboard);
 
+        avatarImage = (ImageView) root.findViewById(R.id.registrationPage_avatarImage);
         Button avatarButton = (Button) root.findViewById(R.id.registrationPage_avatarButton);
-        avatarButton.setOnClickListener(arg0 -> {
-//                Intent i = new Intent(
-//                        Intent.ACTION_PICK,
-//                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//
-//                startActivityForResult(i, RESULT_LOAD_IMAGE);
+        avatarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
         });
 
         Button registerButton = (Button) root.findViewById(R.id.registrationPage_registerButton);
-        registerButton.setOnClickListener(view -> {
-            profileViewModel.setUser();
-            navController.navigate(R.id.nav_home);
-            ((AppCompatActivity)getActivity()).getSupportActionBar().show(); //show toolbar
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profileViewModel.setUser();
+                navController.navigate(R.id.nav_home);
+                ((AppCompatActivity)getActivity()).getSupportActionBar().show(); //show toolbar
+            }
         });
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide(); //hide toolbar
@@ -78,27 +95,12 @@ public class RegistrationFragment extends Fragment {
         return root;
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-//            Uri selectedImage = data.getData();
-//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-//
-//            Cursor cursor = getContentResolver().query(selectedImage,
-//                    filePathColumn, null, null, null);
-//            cursor.moveToFirst();
-//
-//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-//            String picturePath = cursor.getString(columnIndex);
-//            cursor.close();
-//
-//            ImageView imageView = (ImageView) findViewById(R.id.imgView);
-//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-//
-//        }
-//
-//
-//    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            avatarImage.setImageURI(imageUri);
+        }
+    }
 }
